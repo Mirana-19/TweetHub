be10b90d418cd4e8d68bc0bb1bb926166d15886e
Marta Żak <martysik19@gmail.com>
2024-02-24 22:32:15 +0100

    changes text on home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -20,13 +20,13 @@ const Home = () => {
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              "Explore a vibrant social platform where users share their
-              thoughts and experiences in short, engaging tweets. Connect with
-              others, discover trending topics, and stay updated on the latest
-              conversations. Whether you're here to share your voice or simply
-              scroll through the buzz, our platform provides a dynamic space for
-              expressing ideas and connecting with a diverse community."
+            <p className="mb-5 ">
+              "Discover a dynamic social platform where users share thoughts and
+              experiences in short, engaging tweets. Connect, explore trending
+              topics, and stay updated on the latest conversations. Whether
+              sharing your voice or scrolling through the buzz, our platform
+              offers a vibrant space for expressing ideas and connecting with a
+              diverse community."
             </p>
             <button className="btn btn-primary" onClick={handleGetStartedClick}>
               Get Started
